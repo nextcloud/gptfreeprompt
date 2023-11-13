42b8edd25ad7a0bdb8daa3312d899ee3c963571b
MB-Finski <sami.finnila@gmail.com>
2023-11-13 11:06:11 +0000

    Fix licensing
    Signed-off-by: MB-Finski <sami.finnila@gmail.com>

--- lib/Migration/Version010000Date20230929162000.php
+++ lib/Migration/Version010000Date20230929162000.php
@@ -1,5 +1,6 @@
 <?php
-
+// SPDX-FileCopyrightText: Sami Finnilä <sami.finnila@nextcloud.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
 declare(strict_types=1);
 
 namespace OCA\GptFreePrompt\Migration;
--- src/views/GptFreePromptCustomPickerElement.vue
+++ src/views/GptFreePromptCustomPickerElement.vue
@@ -1,3 +1,7 @@
+<!--
+SPDX-FileCopyrightText: Sami Finnilä <sami.finnila@nextcloud.com>
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 <template>
 	<div class="gptfreeprompt-picker-content-wrapper">
 		<div v-if="!pickerEnabled" class="gptfreeprompt-picker-content">
